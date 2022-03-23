7276378484e477a138b9aea9cf0e0a81cc1676fb
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2022-03-23 18:39:17 -0400

    Add run method & begin testing.

--- jspec/lib/Runner.java
+++ jspec/lib/Runner.java
@@ -1,9 +1,30 @@
 package jspec.lib;
 
-class Runner {
-  private Group[] groups;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class Runner {
+  private ArrayList<Group> groups;
+  private ArrayList<Result> results;
 
   public Runner(Group ...groups) {
-    this.groups = groups;
+    this.groups = new ArrayList<Group>(Arrays.asList(groups));
+  }
+
+  public Runner addGroup(Group group) {
+    this.groups.add(group);
+
+    return this;
+  }
+
+  public ArrayList<Group> getGroups() {
+    return this.groups;
+  }
+
+  public ArrayList<Result> run() {
+    this.results = new ArrayList<Result>();
+    groups.forEach(group -> this.results.addAll(group.visit()));
+
+    return this.results;
   }
 }
--- jspec/lib/RunnerSpec.java
+++ jspec/lib/RunnerSpec.java
@@ -4,15 +4,16 @@ public class RunnerSpec extends Group {
   final String desc = "class: Runner";
 
   public static void main(String[] args) {
-    RunnerSpec runner = new RunnerSpec();
+    RunnerSpec spec = new RunnerSpec();
 
-    runner
+    spec
       .visit()
-      .forEach(
-        result -> System.out.println(result.getDescription() +
-                                     (result.didPass()
-                                       ? "✅"
-                                       : "❌")));
+      .forEach(result -> System.out.println(
+        result.getDescription() + 
+        " " + 
+        (result.didPass() 
+          ? "✅" 
+          : "❌")));
   }
 
   String descConstructorNoArgs = "A Runner can be created w/ no arguments";
@@ -34,5 +35,69 @@ public class RunnerSpec extends Group {
     Runner r = new Runner(new Group(), new Group(), new Group(), new Group());
 
     assert r instanceof Runner : "r should be a Runner";
+  }
+
+  String descAddAGroup = "A Group can be added to the Runner later using a Builder pattern";
+  public void testAddAGroup() {
+    Runner r = RunnerFactory.create();
+    Object actual = r.addGroup(new Group());
+
+    assert actual instanceof Runner : "addGroup should return the Runner";
+  }
+
+  String descAddGroupWorks = "Adding a Group to the Runner actually works";
+  public void testAddGroupWorks() {
+    Runner r = RunnerFactory.create();
+    Group g = new Group();
+    r.addGroup(g);
+
+    assert r.getGroups().contains(g);
+  }
+
+  String descRunningTests = "A Runner can run tests for a given Group";
+  public void testRunningTests() {
+    class G extends Group {
+      public void testATest() {
+        assert true;
+      }
+    }
+
+    Runner r = RunnerFactory.create();
+    r.addGroup(new G());
+
+    assert r.run().get(0).didPass();
+  }
+
+  String descRunningTestsForMany = "A Runner can run tests for all given Groups";
+  public void testRunningTestsForMany() {
+    class G1 extends Group {
+      public void testATest() {
+        assert true;
+      }
+
+      public void testAnother() {
+        assert false;
+      }
+    }
+
+    class G2 extends Group {
+      public void testATest() {
+        assert true;
+      }
+    }
+
+    Runner r = RunnerFactory.create();
+    r.addGroup(new G1());
+    r.addGroup(new G2());
+
+    assert r.run().get(0).didPass();
+    assert !r.run().get(1).didPass();
+    assert r.run().get(2).didPass();
+  }
+}
+
+class RunnerFactory {
+  public static Runner create() {
+    return new Runner();
   }
 }
